53e56b511fdd0ad70ed2aa3893238c63885a0c75
Abishek Ramesh <abishekmr721@gmail.com>
2024-02-10 16:42:29 -0500

    Second push

---
+++ File1.py
@@ -0,0 +1,14 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+# Get user input
+num = int(input("Enter a number: "))
+
+# Check if the number is negative
+if num < 0:
+    print("Factorial is not defined for negative numbers.")
+else:
+    print("Factorial of", num, "is", factorial(num))
